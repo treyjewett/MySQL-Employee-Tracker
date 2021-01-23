2419a1bce4df83fb1d1a58d3e1c10ca05bec6095
Russell Jewett <rfjewett3@gmail.com>
2021-01-22 23:28:57 -0500

    adds inquirer logic and async await commands.

--- employeeManager.js
+++ employeeManager.js
@@ -1,5 +1,7 @@
 const mysql = require('mysql');
 const inquirer = require('inquirer');
+const consoleTable = require('console.table');
+const util = require('util');
 
 // Create the connection to MySQL WorkBench
 let connection = mysql.createConnection({
@@ -13,12 +15,161 @@ let connection = mysql.createConnection({
     database: 'employee_DB'
 });
 
+connection.query = util.promisify(connection.query);
+
 // Begin the application after establishing the connection.
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) throw err;
-    runSearch();
+    initialAction();
 })
 
-runSearch = () => {
-    console.log('Connection Made!');
+const initialAction = async () => {
+    try {
+        let answer = await inquirer.prompt({
+            name: 'action',
+            type: 'rawlist',
+            message: 'What would you like to do?',
+            choices: [
+                'View Employees',
+                'View Departments',
+                'View Roles',
+                'Add Employees',
+                'Add Departments',
+                'Add Roles',
+                'Update Employee Role',
+                'Exit'
+            ]
+        });
+        switch (answer.action) {
+            case 'View Employees':
+                employeeView();
+                break;
+
+            case 'View Departments':
+                departmentView();
+                break;
+
+            case 'View Roles':
+                roleView();
+                break;
+
+            case 'Add Employees':
+                employeeAdd();
+                break
+
+            case 'Add Departments':
+                departmentAdd();
+                break
+
+            case 'Add Roles':
+                roleAdd();
+                break
+
+            case 'Update Employee Role':
+                employeeUpdate();
+                break
+
+            case 'Exit':
+                connection.end();
+                break;
+        };
+    } catch (err) {
+        console.log(err);
+        initialAction();
+    };
+}
+
+const employeeView = async () => {
+    console.log('Employee View');
+    try {
+        let query = 'SELECT * FROM employee';
+        connection.query(query, function (err, res) {
+            console.table(res);
+            initialAction();
+        });
+    } catch (err) {
+        console.log(err);
+        initialAction();
+    };
+}
+
+const departmentView = async () => {
+    console.log('Department View');
+    try {
+        let query = 'SELECT * FROM department';
+        connection.query(query, function(err, res) {
+            console.table(res);
+            initialAction();
+        });
+    } catch (err) {
+        console.log(err);
+        initialAction();
+    };
+}
+
+const roleView = async () => {
+    console.log('Role View');
+    try {
+        let query = 'SELECT * FROM role';
+        connection.query(query, function(err, res) {
+            console.table(res);
+            initialAction();
+        });
+    } catch (err) {
+        console.log(err);
+        initialAction();
+    }
+}
+
+const employeeAdd = async () => {
+    try{
+        console.log('Employee Add');
+        let answer = await inquirer.prompt([
+            {
+                name: 'firstName',
+                type: 'input',
+                message: 'What is the first name of this Employee?'
+            },
+            {
+                name: 'lastName',
+                type: 'input',
+                message: 'What is the last name of this Employee?'
+            },
+            {
+                name: 'employeeRoleId',
+                type: 'rawlist',
+                message: "What is this Employee's role id?",
+                choices: ['1','2','3','4']
+            },
+            {
+                name: 'employeeManagerId',
+                type: 'rawlist',
+                message: "What is this Employee's Manager's Id?",
+                choices: ['1','2','3','4']
+            }
+        ])
+        let result = await connection.query("INSERT INTO employee SET ?",{
+                    first_name: answer.firstName,
+                    last_name: answer.lastName,
+                    role_id: answer.employeeRoleId,
+                    manager_id: answer.employeeManagerId
+        });
+        console.log('Employee added successfully!');
+        initialAction();
+    } catch (err) {
+        console.log(err);
+        initialAction();
+    }
+}
+
+function departmentAdd() {
+    console.log('Department Add');
+}
+
+function roleAdd() {
+    console.log('Role Add');
+}
+
+function employeeUpdate() {
+    console.log('Employee Update');
 }
--- employeeSchema.sql
+++ employeeSchema.sql
@@ -2,3 +2,26 @@ DROP DATABASE IF EXISTS employee_DB;
 CREATE database employee_DB;
 
 USE employee_DB;
+
+CREATE TABLE department (
+    id INT NOT NULL,
+    department_name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
+)
+
+CREATE TABLE role (
+    id INT AUTO_INCREMENT NOT NULL,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT,
+    PRIMARY KEY (id)
+)
+
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT,
+    PRIMARY KEY (id)
+)
